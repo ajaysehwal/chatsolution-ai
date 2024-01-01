034cc8845ea600748ce1b2c386dc128e0c752ae3
ajaysehwal <ajaysehwal000@gmail.com>
2024-01-01 22:57:13 +0530

    app runner files---

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env:{
+    NEXT_PUBLIC_OPENAI_API_KEY:"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",
+
+    },
+   
+}
+
+module.exports = nextConfig
