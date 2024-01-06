02deeb86c76dfd0ca7c15148c8cbc2f5d21c1381
ajaysehwal <ajaysehwal000@gmail.com>
2024-01-06 20:18:17 +0530

    perf:sheletonUi changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,20 @@
-"use client";
-import React, { useEffect, useState } from "react";
 import { Suspense } from "react";
 import {SkeletonUI,Main} from "./components";
-import { AuthContext } from "./contexts";
-import { useRouter } from "next/navigation";
-import { generateCode } from "./libs";
-export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-  const router=useRouter();
-  const {AuthState}:any =React.useContext(AuthContext);
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
-  }, []);
-  if(!AuthState){
-    return router.push('/login');
-  }
-  return (
+import {createServerComponentClient} from "@supabase/auth-helpers-nextjs"
+import {cookies} from "next/headers";
+import { redirect } from "next/navigation";
+export default async function Home() {
+  const cookieStore=cookies();
+  const supabase=createServerComponentClient({cookies:()=>cookieStore});
+  const {data:{user}}=await supabase.auth.getUser();
+   if(!user){
+   return redirect('/login');
+   }
+ return (
     <main className="min-h-screen h-[100vh] bg-[rgb(52,53,65)]">
       <Suspense fallback={<SkeletonUI />}>
-        {isLoading ? <SkeletonUI /> : <Main />}
+         <Main />
       </Suspense>
-
     </main>
   );
 }
