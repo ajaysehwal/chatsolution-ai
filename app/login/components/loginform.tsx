dde0b2190a33f65e4415e9c96dec69562e9c6b53
ajaysehwal <ajaysehwal000@gmail.com>
2024-01-06 20:14:10 +0530

    feat:handle non auth users

--- app/login/components/loginform.tsx
+++ app/login/components/loginform.tsx
@@ -1,112 +1,122 @@
-"use client"
-
-import * as React from "react"
-
-import { cn } from "@/lib/utils"
-import { Icons } from "../../components/icons"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import {GoogleAuth} from "../../services"
-import { Login } from "../../services"
+"use client";
+import * as React from "react";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import { Icons } from "../../components/icons";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { GoogleAuth } from "../../services";
+import { Login } from "../../services";
 import { useToast } from "@/components/ui/use-toast";
 import { Toaster } from "@/components/ui/toaster";
+import { supabase } from "../../libs/supabase";
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
-
 export function SignIn({ className, ...props }: UserAuthFormProps) {
-  const {toast}=useToast();
-  const [isLoading, setIsLoading] = React.useState<boolean>(false)
+  const { toast } = useToast();
+  const router = useRouter();
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
   const [email, setEmail] = React.useState<string>("");
   const [password, setPassword] = React.useState<string>("");
-  const login=new Login();
-  const handlelogin=async()=>{
-    try{
-      const response=await login.loginUser(email,password);
-      console.log(response);
-      if(response.user.user==null){
+  const CheckInputValidation = new Login();
+  const handleSignIn = async () => {
+    try {
+      const res = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+      setEmail("");
+      setPassword("");
+
+      if (!res.data.session) {
         toast({
           variant: "destructive",
-          title:response.error,
+          title: res.error?.message,
         });
+      } else {
+        return router.push("/", { scroll: false });
       }
-     
-    }catch(err){
+    } catch (err) {
+      throw new Error(JSON.stringify(err));
+    }
+  };
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    const check = await CheckInputValidation.loginValid(email, password);
+    if (check.status) {
+      await handleSignIn();
+    } else {
+      setIsLoading(true);
       toast({
         variant: "destructive",
-        title:JSON.stringify(err),
+        title: check.response,
       });
-      console.log(err);
     }
-  }
-  async function onSubmit(event: React.SyntheticEvent) {
-    event.preventDefault()
-    setIsLoading(true)
-    await handlelogin();
+    setIsLoading(true);
     setTimeout(() => {
-      setIsLoading(false)
-    }, 3000)
+      setIsLoading(false);
+    }, 3000);
   }
 
   return (
     <>
-    <Toaster/>
-     <div className={cn("grid gap-6", className)} {...props}>
-      <form className="w-[80%] m-auto" onSubmit={onSubmit}>
-        <div className="grid gap-2">
-          <div className="grid gap-1">
-            <Label className="sr-only" htmlFor="email">
-              Email
-            </Label>
-            <Input
-              onChange={(e)=>setEmail(e.target.value)}
-              id="email"
-              placeholder="name@example.com"
-              type="email"
-              value={email}
-              autoCapitalize="none"
-              autoComplete="email"
-              autoCorrect="off"
-              disabled={isLoading}
-            />
+      <Toaster />
+      <div className={cn("grid gap-6", className)} {...props}>
+        <form className="w-[80%] m-auto" onSubmit={onSubmit}>
+          <div className="grid gap-2">
+            <div className="grid gap-1">
+              <Label className="sr-only" htmlFor="email">
+                Email
+              </Label>
+              <Input
+                onChange={(e) => setEmail(e.target.value)}
+                id="email"
+                placeholder="name@example.com"
+                type="email"
+                value={email}
+                autoCapitalize="none"
+                autoComplete="email"
+                autoCorrect="off"
+                disabled={isLoading}
+              />
+            </div>
+            <div className="grid gap-1">
+              <Label className="sr-only" htmlFor="email">
+                Password
+              </Label>
+              <Input
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                placeholder="*******"
+                type="password"
+                autoCapitalize="none"
+                autoComplete="password"
+                autoCorrect="off"
+                disabled={isLoading}
+              />
+            </div>
+
+            <Button disabled={isLoading}>
+              {isLoading && (
+                <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+              )}
+              Sign In with Email
+            </Button>
           </div>
-          <div className="grid gap-1">
-            <Label className="sr-only" htmlFor="email">
-              Password
-            </Label>
-            <Input
-              id="password"
-              onChange={(e)=>setPassword(e.target.value)}
-              value={password}
-              placeholder="*******"
-              type="password"
-              autoCapitalize="none"
-              autoComplete="password"
-              autoCorrect="off"
-              disabled={isLoading}
-            />
+        </form>
+        <div className="relative">
+          <div className="absolute inset-0 flex items-center">
+            <span className="w-full border-t" />
           </div>
-         
-          <Button disabled={isLoading}>
-            {isLoading && (
-              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-            )}
-            Sign In with Email
-          </Button>
+          <div className="relative flex justify-center text-xs uppercase">
+            <span className="bg-background px-2 text-muted-foreground">
+              Or continue with
+            </span>
+          </div>
         </div>
-      </form>
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
-        </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
-        </div>
+        <GoogleAuth />
       </div>
-       <GoogleAuth/>
-    </div>
     </>
-   
-  )
+  );
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,20 +1,59 @@
 "use client";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import { SignIn } from "./components";
 import { motion } from "framer-motion";
-import { useContext } from "react";
-import { AuthContext,AuthContextProps } from "../contexts";
-import { useRouter } from "next/navigation";
+import { useAuth } from "../hooks";
+import Authloading from "../components/authloading";
 
 export default function Login() {
-  const router = useRouter();
-
-  const {AuthState}:any=useContext<AuthContextProps | null>(AuthContext)  
-   if(AuthState){
-     router.push('/', { scroll: false })
-
-   }
-return (
+  
+  const { authState, authload,logOut }: { authState: boolean; authload: boolean,logOut:any } =
+    useAuth();
+  if (authload) {
+    return <Authloading />;
+  }
+  if (authState) {
+    return (
+      <div
+        className="relative h-[100vh] m-auto flex sm:block place-items-center items-center justify-center md:grid lg:max-w-none lg:px-0"
+        style={{
+          background:
+            "radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)",
+        }}
+      >
+        <motion.div
+          initial={{ scale: 0, y: 0 }}
+          animate={{ scale: 1 }}
+          transition={{
+            duration: 2,
+            ease: "backInOut",
+            times: [0, 0.25, 0.5, 0.85, 1],
+          }}
+          exit={{
+            scale: 0,
+            y: 0,
+          }}
+          className="mx-auto w-full  justify-center place-items-center items-center space-y-6  py-7 px-10 rounded-xl"
+          style={{
+            boxShadow:
+              "rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px",
+          }}
+        >
+          <h1 className="mb-4 text-xl font-bold text-gray-700 dark:text-gray-300">
+            You're already logged in
+          </h1>
+          <Button
+            onClick={() => logOut()}
+            className="w-full bg-red-600 text-white hover:bg-transparent hover:text-red-500 border-red-500 font-semibold"
+          >
+            Log out
+          </Button>
+        </motion.div>
+      </div>
+    );
+  }
+  return (
     <>
       <div
         className="relative h-[100vh] m-auto flex sm:block place-items-center items-center justify-center md:grid lg:max-w-none lg:px-0"
@@ -43,12 +82,10 @@ return (
         >
           <div className="space-y-2 text-center ">
             <h1 className="text-2xl font-semibold tracking-tight">
-             Welcome back!
+              Welcome back!
             </h1>
             <p className="text-sm text-muted-foreground">
-             
-                Enter your email and password below to login your account
-                
+              Enter your email and password below to login your account
             </p>
           </div>
 
@@ -65,19 +102,18 @@ return (
               y: 0,
             }}
           >
-           <SignIn /> 
+            <SignIn />
           </motion.div>
 
           <p className="px-8 text-center text-sm text-muted-foreground">
             {"Don't have an account?"}
-          
-              <Link
-                href="/register"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Register
-              </Link>
-            
+
+            <Link
+              href="/register"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Register
+            </Link>
           </p>
         </motion.div>
       </div>
