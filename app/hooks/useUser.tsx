ec109c7d3c92274202b20082acb5a424ebb5fdbe
ajaysehwal <ajaysehwal000@gmail.com>
2024-01-07 21:11:16 +0530

    perf : improve useUser hook

--- app/hooks/useUser.tsx
+++ app/hooks/useUser.tsx
@@ -1,18 +1,21 @@
 import { useState, useEffect } from "react";
 import { supabase } from "../libs/supabase";
 export const useUser = () => {
-  const [metadata, setMetadata] = useState<any | null>(null);
-  const [userData,setUserData]=useState({})
+  const [metadata, setMetadata] = useState<any | {}>({});
+  const [userData, setUserData] = useState({});
   const getUserMetaData = async () => {
     try {
-      const { data: { user } } = await supabase.auth.getUser();
-      if (user) {
-        console.log(user);
-        setUserData(user)
-        setMetadata(user.user_metadata);
-      } else {
-        setMetadata({ message: "Error in Supabase data fetch.." });
-      }
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setTimeout(() => {
+        if (user) {
+          setUserData(user);
+          setMetadata(user.user_metadata);
+        } else {
+          setMetadata({ message: "Error in Supabase data fetch.." });
+        }
+      }, 1000);
     } catch (error) {
       throw new Error(JSON.stringify({ GETTING_USER_AUTH_DATA_ERROR: error }));
     }
@@ -22,5 +25,5 @@ export const useUser = () => {
     getUserMetaData();
   }, []);
 
-  return { metadata,userData };
+  return { metadata, userData };
 };
