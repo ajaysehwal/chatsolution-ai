0630b9c346c2399a32ed1c7c68a5873e3e07cccc
ajaysehwal <ajaysehwal000@gmail.com>
2024-01-08 21:58:06 +0530

    perf : make some improvements

--- app/components/message-input.tsx
+++ app/components/message-input.tsx
@@ -2,33 +2,39 @@ import { Input } from "@/components/ui/input";
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { ArrowUpIcon } from "@radix-ui/react-icons";
-import {motion} from "framer-motion"
-interface PropsInterface{
-  handleMessageGenerate:()=>Promise<void>;
-  setmessage:React.Dispatch<React.SetStateAction<string>>;
-  message:string;
-
+import { motion } from "framer-motion";
+interface PropsInterface {
+  handleMessageGenerate: () => Promise<void>;
+  setmessage: React.Dispatch<React.SetStateAction<string>>;
+  message: string;
 }
-export default function MessageInput(props:PropsInterface) {
-  const {handleMessageGenerate,setmessage,message}=props
- const handleSubmit=async(e:any)=>{
+export default function MessageInput(props: PropsInterface) {
+  const { handleMessageGenerate, setmessage, message } = props;
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
-   await handleMessageGenerate();
-    setTimeout(()=>{
-      setmessage('');
-    },1000)
-  }
+    await handleMessageGenerate();
+    setTimeout(() => {
+      setmessage("");
+    }, 1000);
+  };
   return (
-    <motion.div initial={{x:2,scale:0}} animate={{scale:1,x:0}}  className="fixed w-[90%] lg:w-[80%] md:w-[85%] bottom-5 m-auto">
+    <motion.div
+      initial={{ x: 2, scale: 0 }}
+      animate={{ scale: 1, x: 0 }}
+      className="fixed w-[90%] lg:w-[80%] md:w-[85%] bottom-5 m-auto"
+    >
       <form className="flex align-middle" onSubmit={handleSubmit}>
         <Input
           value={message}
-          onChange={(e)=>setmessage(e.target.value)}
+          onChange={(e) => setmessage(e.target.value)}
           className="w-[90%] lg:w-[70%] md:w-[80%]  m-auto p-5 h-[55px] bg-[rgb(52,53,65)] text-white"
           style={{ border: "1px solid rgb(154,154,160)" }}
           placeholder="Message ChatGPT..."
         />
-        <Button type="submit" className="absolute right-[9%] lg:right-[16%] md:right-[12%] mt-2">
+        <Button
+          type="submit"
+          className="absolute right-[9%] lg:right-[16%] md:right-[12%] mt-2"
+        >
           <ArrowUpIcon />
         </Button>
       </form>
