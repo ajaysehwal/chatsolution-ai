cd2f57537966e89779222f401450e6876373488e
ajaysehwal <ajaysehwal000@gmail.com>
2024-01-10 21:58:13 +0530

    perf : make some minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { AuthProvider } from './contexts'
+import { AppProvider } from './contexts'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -18,9 +18,9 @@ export default function RootLayout({
     <html lang="en">
 
       <body className={inter.className}>
-        <AuthProvider>
+        <AppProvider>
         {children}
-        </AuthProvider>
+        </AppProvider>
         </body>
     </html>
   )
