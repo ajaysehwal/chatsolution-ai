946f29114b0b16fa92b0e1518b35571836e76778
ajaysehwal <ajaysehwal000@gmail.com>
2024-01-10 21:58:44 +0530

    perf : make some minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import {SkeletonUI,Main} from "./components";
 import {createServerComponentClient} from "@supabase/auth-helpers-nextjs"
 import {cookies} from "next/headers";
 import { redirect } from "next/navigation";
+
 export default async function Home() {
-  
   const cookieStore=cookies();
   const supabase=createServerComponentClient({cookies:()=>cookieStore});
   const {data:{user}}=await supabase.auth.getUser();
