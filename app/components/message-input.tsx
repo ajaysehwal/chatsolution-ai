601e26aefdb3de2363dbfb33865d2bedd242e87d
ajaysehwal <ajaysehwal000@gmail.com>
2024-01-11 22:27:45 +0530

    feat : add loader on message button

--- app/components/message-input.tsx
+++ app/components/message-input.tsx
@@ -1,15 +1,17 @@
 import { Input } from "@/components/ui/input";
-import React, { useState } from "react";
+import React from "react";
 import { Button } from "@/components/ui/button";
 import { ArrowUpIcon } from "@radix-ui/react-icons";
 import { motion } from "framer-motion";
+import { Loader2 } from "lucide-react";
 interface PropsInterface {
   handleMessageGenerate: () => Promise<void>;
   setmessage: React.Dispatch<React.SetStateAction<string>>;
   message: string;
+  load: boolean;
 }
 export default function MessageInput(props: PropsInterface) {
-  const { handleMessageGenerate, setmessage, message } = props;
+  const { handleMessageGenerate, setmessage, message, load } = props;
   const handleSubmit = async (e: any) => {
     e.preventDefault();
     await handleMessageGenerate();
@@ -30,12 +32,17 @@ export default function MessageInput(props: PropsInterface) {
           className="w-[90%] lg:w-[70%] md:w-[80%]  m-auto p-5 h-[55px] bg-[rgb(52,53,65)] text-white"
           style={{ border: "1px solid rgb(154,154,160)" }}
           placeholder="Message ChatGPT..."
+          required={true}
         />
         <Button
           type="submit"
           className="absolute right-[9%] lg:right-[16%] md:right-[12%] mt-2"
         >
-          <ArrowUpIcon />
+          {load ? (
+            <Loader2 className="w-4 h-4 animate-spin" />
+          ) : (
+            <ArrowUpIcon />
+          )}
         </Button>
       </form>
       <p className="text-center text-[12px] mt-1 text-[rgb(154,154,160)]">
