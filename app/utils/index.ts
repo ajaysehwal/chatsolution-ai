80e4771c264674c9761db22d6faf7624aaf93f68
ajaysehwal <ajaysehwal000@gmail.com>
2024-01-20 21:25:08 +0530

    feat : store user chat data

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,3 +1,14 @@
-export const ChatDataCollector=(data:{})=>{
-    return data
-}
+import { ManageChat, ChatDataProps } from "../services";
+
+export const handleStoreData = (data: ChatDataProps) => {
+  const manageChat = new ManageChat();
+  return manageChat.storeChat({
+    email: data.email,
+    user_id: data.user_id,
+    chat_id: data.chat_id,
+    chat_message: data.chat_message,
+    chat_response: data.chat_response,
+    access_token: data.access_token,
+    name: data.name,
+  });
+};
