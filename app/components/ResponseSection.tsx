3571d1b4422c8a15082942f11543dc3b47e6b8df
ajaysehwal <ajaysehwal000@gmail.com>
2024-01-21 22:17:49 +0530

    fix : make change in UI

---
+++ app/components/ResponseSection.tsx
@@ -0,0 +1,285 @@
+import React, { useRef } from "react";
+import { motion } from "framer-motion";
+
+export default function ResponseSection({
+  el,
+  chunks,
+}: {
+  el: { isNew: boolean; chat_response: string };
+  chunks: string;
+}) {
+  const textRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const handleCopyClick = () => {
+    try {
+      if (textRef.current) {
+        const range = document.createRange();
+        range.selectNodeContents(textRef.current);
+
+        const selection = window.getSelection();
+        if (selection) {
+          selection.removeAllRanges();
+          selection.addRange(range);
+          document.execCommand("copy");
+          selection.removeAllRanges();
+        }
+      }
+    } catch (error) {
+      throw new Error(
+        "Unable to copy response please you can select text and press Clt + C"
+      );
+    }
+  };
+
+  return (
+    <>
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={{
+          hidden: { opacity: 0, x: "1%" },
+          visible: {
+            opacity: 1,
+            x: 0,
+            transition: { duration: 1, ease: "easeInOut" },
+          },
+        }}
+      
+      >
+        {/* <p
+          className="p-1"
+          ref={textRef as React.RefObject<HTMLParagraphElement>}
+        >
+          {!el.isNew ? el.chat_response : chunks}
+        </p>
+
+        <button
+          onClick={handleCopyClick}
+          className="copy absolute top-2 right-2  copy-button"
+        >
+          <span
+            data-text-end="Copied!"
+            data-text-initial="Copy to clipboard"
+            className="tooltip"
+          ></span>
+          <span>
+            <svg
+              xmlSpace="preserve"
+              viewBox="0 0 6.35 6.35"
+              y="0"
+              x="0"
+              height="20"
+              width="20"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              className="clipboard"
+            >
+              <g>
+                <path
+                  fill="currentColor"
+                  d="M2.43.265c-.3 0-.548.236-.573.53h-.328a.74.74 0 0 0-.735.734v3.822a.74.74 0 0 0 .735.734H4.82a.74.74 0 0 0 .735-.734V1.529a.74.74 0 0 0-.735-.735h-.328a.58.58 0 0 0-.573-.53zm0 .529h1.49c.032 0 .049.017.049.049v.431c0 .032-.017.049-.049.049H2.43c-.032 0-.05-.017-.05-.049V.843c0-.032.018-.05.05-.05zm-.901.53h.328c.026.292.274.528.573.528h1.49a.58.58 0 0 0 .573-.529h.328a.2.2 0 0 1 .206.206v3.822a.2.2 0 0 1-.206.205H1.53a.2.2 0 0 1-.206-.205V1.529a.2.2 0 0 1 .206-.206z"
+                ></path>
+              </g>
+            </svg>
+            <svg
+              xmlSpace="preserve"
+              viewBox="0 0 24 24"
+              y="0"
+              x="0"
+              height="18"
+              width="18"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              className="checkmark"
+            >
+              <g>
+                <path
+                  data-original="#000000"
+                  fill="currentColor"
+                  d="M9.707 19.121a.997.997 0 0 1-1.414 0l-5.646-5.647a1.5 1.5 0 0 1 0-2.121l.707-.707a1.5 1.5 0 0 1 2.121 0L9 14.171l9.525-9.525a1.5 1.5 0 0 1 2.121 0l.707.707a1.5 1.5 0 0 1 0 2.121z"
+                ></path>
+              </g>
+            </svg>
+          </span>
+        </button> */}
+        <li className="max-w-4xl py-2 px-4 sm:px-6 lg:px-8 mx-auto flex gap-x-2 sm:gap-x-4">
+              <svg
+                className="flex-shrink-0 w-[2.375rem] h-[2.375rem] rounded-full"
+                width="38"
+                height="38"
+                viewBox="0 0 38 38"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect width="38" height="38" rx="6" fill="#2563EB" />
+                <path
+                  d="M10 28V18.64C10 13.8683 14.0294 10 19 10C23.9706 10 28 13.8683 28 18.64C28 23.4117 23.9706 27.28 19 27.28H18.25"
+                  stroke="white"
+                  stroke-width="1.5"
+                />
+                <path
+                  d="M13 28V18.7552C13 15.5104 15.6863 12.88 19 12.88C22.3137 12.88 25 15.5104 25 18.7552C25 22 22.3137 24.6304 19 24.6304H18.25"
+                  stroke="white"
+                  stroke-width="1.5"
+                />
+                <ellipse cx="19" cy="18.6554" rx="3.75" ry="3.6" fill="white" />
+              </svg>
+
+              <div className="grow max-w-[90%] md:max-w-2xl w-full space-y-3">
+                {/* <!-- Card --> */}
+                <div className="space-y-3">
+                  <p className="text-sm text-gray-800 dark:text-white">
+                  {!el.isNew ? el.chat_response : chunks}
+                  </p>
+                  {/* <div className="space-y-1.5">
+                    <p className="text-sm text-gray-800 dark:text-white">
+                      Here,re some links to get started
+                    </p>
+                    <ul>
+                      <li>
+                        <a
+                          className="text-sm text-blue-600 decoration-2 hover:underline font-medium dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                          href="../docs/index.html"
+                        >
+                          Installation Guide
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          className="text-sm text-blue-600 decoration-2 hover:underline font-medium dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                          href="../docs/frameworks.html"
+                        >
+                          Framework Guides
+                        </a>
+                      </li>
+                    </ul>
+                  </div> */}
+                </div>
+                {/* <!-- End Card -->
+
+          <!-- Button Group --> */}
+                <div>
+                  <div className="sm:flex sm:justify-between">
+                    <div>
+                      <div className="inline-flex border border-gray-200 rounded-full p-0.5 dark:border-gray-700">
+                        <button
+                          type="button"
+                          className="inline-flex flex-shrink-0 justify-center items-center h-8 w-8 rounded-full text-gray-500 hover:bg-blue-100 hover:text-blue-800 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:hover:bg-blue-900 dark:hover:text-blue-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                        >
+                          <svg
+                            className="flex-shrink-0 h-4 w-4"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          >
+                            <path d="M7 10v12" />
+                            <path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2h0a3.13 3.13 0 0 1 3 3.88Z" />
+                          </svg>
+                        </button>
+                        <button
+                          type="button"
+                          className="inline-flex flex-shrink-0 justify-center items-center h-8 w-8 rounded-full text-gray-500 hover:bg-blue-100 hover:text-blue-800 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:hover:bg-blue-900 dark:hover:text-blue-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                        >
+                          <svg
+                            className="flex-shrink-0 h-4 w-4"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          >
+                            <path d="M17 14V2" />
+                            <path d="M9 18.12 10 14H4.17a2 2 0 0 1-1.92-2.56l2.33-8A2 2 0 0 1 6.5 2H20a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-2.76a2 2 0 0 0-1.79 1.11L12 22h0a3.13 3.13 0 0 1-3-3.88Z" />
+                          </svg>
+                        </button>
+                      </div>
+                      <button
+                        type="button"
+                        className="py-2 px-3 inline-flex items-center gap-x-2 text-sm rounded-full border border-transparent text-gray-500 hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                      >
+                        <svg
+                          className="flex-shrink-0 h-4 w-4"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        >
+                          <path d="M17 14V2" />
+                          <path d="M9 18.12 10 14H4.17a2 2 0 0 1-1.92-2.56l2.33-8A2 2 0 0 1 6.5 2H20a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-2.76a2 2 0 0 0-1.79 1.11L12 22h0a3.13 3.13 0 0 1-3-3.88Z" />
+                        </svg>
+                        Copy
+                      </button>
+                      <button
+                        type="button"
+                        className="py-2 px-3 inline-flex items-center gap-x-2 text-sm rounded-full border border-transparent text-gray-500 hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                      >
+                        <svg
+                          className="flex-shrink-0 h-4 w-4"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        >
+                          <circle cx="18" cy="5" r="3" />
+                          <circle cx="6" cy="12" r="3" />
+                          <circle cx="18" cy="19" r="3" />
+                          <line x1="8.59" x2="15.42" y1="13.51" y2="17.49" />
+                          <line x1="15.41" x2="8.59" y1="6.51" y2="10.49" />
+                        </svg>
+                        Share
+                      </button>
+                    </div>
+
+                    <div className="mt-1 sm:mt-0">
+                      <button
+                        type="button"
+                        className="py-2 px-3 inline-flex items-center gap-x-2 text-sm rounded-full border border-transparent text-gray-500 hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                      >
+                        <svg
+                          className="flex-shrink-0 h-4 w-4"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        >
+                          <path d="M21 12a9 9 0 1 1-9-9c2.52 0 4.93 1 6.74 2.74L21 8" />
+                          <path d="M21 3v5h-5" />
+                        </svg>
+                        New answer
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        </li>
+      </motion.div>
+    </>
+  );
+}
