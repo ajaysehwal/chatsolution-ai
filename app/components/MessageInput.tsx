6d4516e71817330e0a2fdbeb1340c6ec4ff830f5
ajaysehwal <ajaysehwal000@gmail.com>
2024-01-21 22:20:35 +0530

    fix : change input layout

---
+++ app/components/MessageInput.tsx
@@ -0,0 +1,183 @@
+import { Input } from "@/components/ui/input";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
+import { motion } from "framer-motion";
+import { Loader2 } from "lucide-react";
+interface PropsInterface {
+  onSubmit: () => Promise<void>;
+  setmessage: React.Dispatch<React.SetStateAction<string>>;
+  message: string;
+  load: boolean;
+}
+export default function MessageInput(props: PropsInterface) {
+  const { onSubmit, setmessage, message, load } = props;
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    await onSubmit();
+  };
+  return (
+      <footer className="max-w-4xl mx-auto sticky bottom-0 z-10 p-3 sm:py-6">
+        <div className="lg:hidden flex justify-end mb-2 sm:mb-3">
+          <button
+            type="button"
+            className="p-2 inline-flex items-center gap-x-2 text-xs font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+            data-hs-overlay="#application-sidebar"
+            aria-controls="application-sidebar"
+            aria-label="Toggle navigation"
+          >
+            <svg
+              className="flex-shrink-0 w-3.5 h-3.5"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <line x1="3" x2="21" y1="6" y2="6" />
+              <line x1="3" x2="21" y1="12" y2="12" />
+              <line x1="3" x2="21" y1="18" y2="18" />
+            </svg>
+            <span>Sidebar</span>
+          </button>
+        </div>
+
+        <div className="relative">
+          <div className="flex justify-between items-center mb-3">
+            <div></div>
+
+            <button
+              type="button"
+              className="py-1.5 px-2 inline-flex items-center gap-x-2 text-xs font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+            >
+              <svg
+                className="w-3 h-3"
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path d="M5 3.5h6A1.5 1.5 0 0 1 12.5 5v6a1.5 1.5 0 0 1-1.5 1.5H5A1.5 1.5 0 0 1 3.5 11V5A1.5 1.5 0 0 1 5 3.5z" />
+              </svg>
+              Stop generating
+            </button>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <textarea
+              value={message}
+              onChange={(e) => setmessage(e.target.value)}
+              className="p-4 pb-12 block w-full bg-gray-100 border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+              placeholder="Ask me anything..."
+            ></textarea>
+            <div className="absolute bottom-px inset-x-px p-2 rounded-b-md bg-gray-100 dark:bg-slate-800">
+              <div className="flex justify-between items-center">
+                {/* <!-- Button Group --> */}
+                <div className="flex items-center">
+                  {/* <!-- Mic Button --> */}
+                  <button
+                    type="button"
+                    className="inline-flex flex-shrink-0 justify-center items-center h-8 w-8 rounded-lg text-gray-500 hover:text-blue-600 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:hover:text-blue-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                  >
+                    <svg
+                      className="flex-shrink-0 h-4 w-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <rect width="18" height="18" x="3" y="3" rx="2" />
+                      <line x1="9" x2="15" y1="15" y2="9" />
+                    </svg>
+                  </button>
+                  {/* <!-- End Mic Button -->
+
+            <!-- Attach Button --> */}
+                  <button
+                    type="button"
+                    className="inline-flex flex-shrink-0 justify-center items-center h-8 w-8 rounded-lg text-gray-500 hover:text-blue-600 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:hover:text-blue-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                  >
+                    <svg
+                      className="flex-shrink-0 h-4 w-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="m21.44 11.05-9.19 9.19a6 6 0 0 1-8.49-8.49l8.57-8.57A4 4 0 1 1 18 8.84l-8.59 8.57a2 2 0 0 1-2.83-2.83l8.49-8.48" />
+                    </svg>
+                  </button>
+                  {/* <!-- End Attach Button --> */}
+                </div>
+
+                <div className="flex items-center gap-x-1">
+                  {/* <!-- Mic Button --> */}
+                  <button
+                    type="button"
+                    className="inline-flex flex-shrink-0 justify-center items-center h-8 w-8 rounded-lg text-gray-500 hover:text-blue-600 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:hover:text-blue-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                  >
+                    <svg
+                      className="flex-shrink-0 h-4 w-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="M12 2a3 3 0 0 0-3 3v7a3 3 0 0 0 6 0V5a3 3 0 0 0-3-3Z" />
+                      <path d="M19 10v2a7 7 0 0 1-14 0v-2" />
+                      <line x1="12" x2="12" y1="19" y2="22" />
+                    </svg>
+                  </button>
+                  {/* <!-- End Mic Button -->
+
+            <!-- Send Button --> */}
+                  <button
+                    type="submit"
+                    className="inline-flex flex-shrink-0 justify-center items-center h-8 w-8 rounded-lg text-white bg-blue-600 hover:bg-blue-500 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                  >
+                    <svg
+                      className="flex-shrink-0 h-3.5 w-3.5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />
+                    </svg>
+                  </button>
+                  {/* <!-- End Send Button --> */}
+                </div>
+                {/* <!-- End Button Group --> */}
+              </div>
+            </div>
+          </form>
+
+          {/* <!-- Toolbar --> */}
+
+          {/* <!-- End Toolbar --> */}
+        </div>
+        {/* <!-- End Input --> */}
+      </footer>
+  
+  );
+}
