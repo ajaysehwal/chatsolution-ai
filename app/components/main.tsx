8d646a58a32a40e9c23249c9b1251ba5a80701e2
ajaysehwal <ajaysehwal000@gmail.com>
2024-01-22 22:05:40 +0530

    .

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -1,29 +1,22 @@
 "use client";
-import ChatHistory from "./ChatHistory";
 import MobileNav from "./MobileNav";
 import Sidebar from "./sidebar";
 import ChatSection from "./ChatSection";
-
 const MainComponent = () => {
   return (
     <div>
-      <MobileNav>
-        <ChatHistory />
-      </MobileNav>
-        <Sidebar />
-        <ChatSection />
-       
+      <MobileNav/>
+      <Sidebar  />
+      <ChatSection />
     </div>
   );
 };
 export async function Main() {
   return (
     <>
-      <div className="relative h-screen w-full lg:ps-64">
-      <div className="py-10 lg:py-14">
-          <MainComponent /> 
-
-         
+      <div  className="relative h-screen w-full lg:ps-64">
+        <div className="py-10 lg:py-14">
+          <MainComponent />
         </div>
       </div>
     </>
