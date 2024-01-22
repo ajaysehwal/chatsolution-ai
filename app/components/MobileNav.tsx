d792888eee4b76d0ec6d90c83d9505e66967a6db
ajaysehwal <ajaysehwal000@gmail.com>
2024-01-22 22:06:58 +0530

    fix : make mobile screen responsive nav

--- app/components/MobileNav.tsx
+++ app/components/MobileNav.tsx
@@ -1,58 +1,76 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import {
   Sheet,
   SheetContent,
   SheetDescription,
   SheetTrigger,
-  SheetFooter
 } from "@/components/ui/sheet";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { AccountMenu } from "./AccountMenu";
-export default function MobileNav({ children }: { children: ReactNode }) {
+import ChatHistory from "./ChatHistory";
+const MobileSideBar = () => {
+  return (
+    <div
+      className="duration-300 transform fixed top-0 start-0 bottom-0 z-[60] w-64 bg-white border-e border-gray-200 overflow-y-auto lg:translate-x-0 lg:end-auto lg:bottom-0 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-slate-700 dark:[&::-webkit-scrollbar-thumb]:bg-slate-500 dark:bg-slate-900 dark:border-gray-700"
+    >
+      <nav
+        className="hs-accordion-group w-full h-full flex flex-col"
+        data-hs-accordion-always-open
+      >
+        <div className="flex items-center justify-between pt-4 pe-4 ps-7 pb-2 text-blue-700 font-bold">
+          CSOL AI
+        </div>
+
+        <div className="h-full overflow-y-scroll">
+          <ul className="space-y-1.5 p-4">
+            <ChatHistory />
+          </ul>
+        </div>
+
+        <div className="mt-auto">
+          <div className="py-2.5 px-7">
+            <p className="inline-flex items-center gap-x-2 text-xs text-green-600">
+              <span className="block w-1.5 h-1.5 rounded-full bg-green-600"></span>
+              Active 12,320 people
+            </p>
+          </div>
+          <AccountMenu />
+        </div>
+      </nav>
+    </div>
+  );
+};
+export default function MobileNav() {
   const MenuDrawer = () => {
     return (
-      <Sheet >
+      <Sheet>
         <SheetTrigger>
-          <HamburgerMenuIcon
-            width="22px"
-            height="22px"
-            color="white"
-            className="text-white"
-          />
+        <div className="ps-3 sm:ps-6 sm:ms-6 sm:border-s sm:border-gray-300 dark:border-gray-700">
+        <button type="button" className="w-9 h-9 flex justify-center items-center text-sm font-semibold rounded-lg border border-gray-200 text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" data-hs-overlay="#navbar-secondary-content" aria-controls="navbar-secondary-content" aria-label="Toggle navigation">
+          <svg className="hs-collapse-open:hidden flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="18" y2="18"/></svg>
+          <svg className="hs-collapse-open:block hidden flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+        </button>
+      </div>
         </SheetTrigger>
-        <SheetContent side={"left"} className="bg-black text-white">
-          <SheetDescription className="bg-black text-white">
-            {children}
-
+        <SheetContent side={"left"}>
+          <SheetDescription>
+            <MobileSideBar />
           </SheetDescription>
-          <SheetFooter>
-            <AccountMenu />
-          </SheetFooter>
         </SheetContent>
-
       </Sheet>
     );
   };
   return (
     <>
-      <div className="w-full flex fixed top-0 sm:flex lg:hidden md:flex bg-[rgb(52,53,65)]" style={{ boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px" }}>
+      <nav className="w-[95%] m-auto flex sm:flex md:flex lg:hidden left-0 right-0 fixed top-1 z-[999] rounded-[36px] bg-gray-100  justify-between items-center px-5">
+      <div>
+          <p className="text-blue-700 font-bold">CSOL AI </p>
+        </div>
         <div className="p-3">
           <MenuDrawer />
         </div>
-        <div className="m-auto">
-          <svg
-            className="mr-10px"
-            xmlns="http://www.w3.org/2000/svg"
-            width="25"
-            fill="white"
-            height="25"
-            preserveAspectRatio="xMidYMid"
-            viewBox="0 0 256 260"
-          >
-            <path d="M239.184 106.203a64.716 64.716 0 0 0-5.576-53.103C219.452 28.459 191 15.784 163.213 21.74A65.586 65.586 0 0 0 52.096 45.22a64.716 64.716 0 0 0-43.23 31.36c-14.31 24.602-11.061 55.634 8.033 76.74a64.665 64.665 0 0 0 5.525 53.102c14.174 24.65 42.644 37.324 70.446 31.36a64.72 64.72 0 0 0 48.754 21.744c28.481.025 53.714-18.361 62.414-45.481a64.767 64.767 0 0 0 43.229-31.36c14.137-24.558 10.875-55.423-8.083-76.483Zm-97.56 136.338a48.397 48.397 0 0 1-31.105-11.255l1.535-.87 51.67-29.825a8.595 8.595 0 0 0 4.247-7.367v-72.85l21.845 12.636c.218.111.37.32.409.563v60.367c-.056 26.818-21.783 48.545-48.601 48.601Zm-104.466-44.61a48.345 48.345 0 0 1-5.781-32.589l1.534.921 51.722 29.826a8.339 8.339 0 0 0 8.441 0l63.181-36.425v25.221a.87.87 0 0 1-.358.665l-52.335 30.184c-23.257 13.398-52.97 5.431-66.404-17.803ZM23.549 85.38a48.499 48.499 0 0 1 25.58-21.333v61.39a8.288 8.288 0 0 0 4.195 7.316l62.874 36.272-21.845 12.636a.819.819 0 0 1-.767 0L41.353 151.53c-23.211-13.454-31.171-43.144-17.804-66.405v.256Zm179.466 41.695-63.08-36.63L161.73 77.86a.819.819 0 0 1 .768 0l52.233 30.184a48.6 48.6 0 0 1-7.316 87.635v-61.391a8.544 8.544 0 0 0-4.4-7.213Zm21.742-32.69-1.535-.922-51.619-30.081a8.39 8.39 0 0 0-8.492 0L99.98 99.808V74.587a.716.716 0 0 1 .307-.665l52.233-30.133a48.652 48.652 0 0 1 72.236 50.391v.205ZM88.061 139.097l-21.845-12.585a.87.87 0 0 1-.41-.614V65.685a48.652 48.652 0 0 1 79.757-37.346l-1.535.87-51.67 29.825a8.595 8.595 0 0 0-4.246 7.367l-.051 72.697Zm11.868-25.58 28.138-16.217 28.188 16.218v32.434l-28.086 16.218-28.188-16.218-.052-32.434Z" />
-          </svg>
-        </div>
-      </div>
+        
+      </nav>
     </>
   );
 }
