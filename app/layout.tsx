e1545016db7490a6f072d89932c4b5cf4dff6068
ajaysehwal <ajaysehwal000@gmail.com>
2024-01-22 22:11:56 +0530

    fix : make some minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,30 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { AppProvider } from './contexts'
-const inter = Inter({ subsets: ['latin'] })
-import PrelineScript from './components/PrelineScript'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { AppProvider } from "./contexts";
+const inter = Inter({ subsets: ["latin"] });
+import PrelineScript from "./components/PrelineScript";
+import Script from "next/script";
 export const metadata: Metadata = {
-  title: 'CSOL-AI',
-  description: 'Experience the future of seamless, intelligent communication with our AI-powered chat solution',
-}
+  title: "CSOL-AI",
+  description:
+    "Experience the future of seamless, intelligent communication with our AI-powered chat solution",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-
       <body className={inter.className}>
         <AppProvider>
-        {children}
+          {children}
+          <PrelineScript />
         </AppProvider>
-        </body>
-        <PrelineScript />
-
+      </body>
+      <Script src="../node_modules/preline/dist/preline.js"></Script>
     </html>
-  )
+  );
 }
